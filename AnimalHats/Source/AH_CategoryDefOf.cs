be01476f85f3495721d2131ed26a026c5c3f8050
Merwini <86023141+Merwini@users.noreply.github.com>
2023-11-09 15:01:22 -0500

    Add project files.

---
+++ AnimalHats/Source/AH_CategoryDefOf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Nuff.AnimalHats
+{
+    [DefOf]
+    class AH_CategoryDefOf
+    {
+        public static ThingCategoryDef Nuff_AnimalHat;
+    }
+}
---
+++ AnimalHats/Source/ThoughtWorker_AnimalHat.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Nuff.AnimalHats
+{
+    public class ThoughtWorker_AnimalHat : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            List<Apparel> wornApparel = p.apparel.WornApparel;
+            for (int i = 0; i < wornApparel.Count; i++)
+            {
+                if (wornApparel[i].def.thingCategories.Contains(AH_CategoryDefOf.Nuff_AnimalHat))
+                {
+                    return true;
+                }
+            }
+
+            return ThoughtState.Inactive;
+        }
+    }
+}
